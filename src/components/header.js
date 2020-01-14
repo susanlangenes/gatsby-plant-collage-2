51328413dfc25a3d0d30deaf6487a045ca7f4935
Susan Langenes <susan@Pauls-MacBook-Pro.local>
2020-01-14 12:01:17 -0800

    change colors

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#4299af`,
       marginBottom: `1.45rem`,
     }}
   >
