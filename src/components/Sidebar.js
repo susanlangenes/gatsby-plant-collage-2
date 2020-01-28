b8015d8b56e47177515ca0aa096f08dd8cac7fd3
Susan Langenes <susan@collagecreative.net>
2020-01-28 08:21:15 -0800

    html output for Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,45 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import { blogURI } from "../../globals"
+import { Link, StaticQuery, graphql } from "gatsby"
 
-const ComponentName = () => (
-  <StaticQuery
-    query={graphql`
-      {
-      wpgraphql {
-        posts {
-          edges {
-            node {
-              id
-              title
-            }
-          }
-        }
+
+
+const RECENT_POSTS_QUERY = graphql`
+query GetRecentPosts {
+  wpgraphql {
+    posts(first: 6) {
+      nodes {
+        title
+        slug
       }
     }
-    `}
-    render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
-  ></StaticQuery>
+  }
+}
+`
+
+    
+    const RecentPostsWidget = () => (
+  <StaticQuery
+    query={RECENT_POSTS_QUERY}
+    render={data => {
+      const { posts } = data.wpgraphql
+      return (
+        <section id="recent-posts-2" className="widget widget_recent_entries">
+          <h2 className="widget-title">Recent Posts</h2>
+          <ul>
+            {posts.nodes.length
+              ? posts.nodes.map(post => (
+                  <li key={post.slug}>
+                    <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+                  </li>
+                ))
+              : null}
+          </ul>
+        </section>
+      )
+    }}
+  />
 )
 
-export default ComponentName
+export default RecentPostsWidget
 
 
